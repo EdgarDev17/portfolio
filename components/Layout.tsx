bcf2351352385fc0c61cdf8da38b8097c6cce92a
Edgardo Rodriguez <62724786+EdgarDev17@users.noreply.github.com>
2022-06-09 21:57:16 -0600

    Making mobile version

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,17 @@
 import NavBar from './NavBar'
 
 type Props = {
-	children: React.ReactNode
+    children: React.ReactNode
 }
 
 const Layout = ({ children }: Props) => {
-	return (
-		<div className={'w-11/12 mx-auto'}>
-			<NavBar />
-			<main>{children}</main>
-		</div>
-	)
+    return (
+        <div className={'w-11/12 mx-auto lg:mx-auto lg:w-11/12'}>
+            <NavBar />
+            <main>
+                {children}
+                </main>
+        </div>
+    )
 }
 export default Layout
--- components/Title.tsx
+++ components/Title.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 
 type Props = {
-	children: React.ReactNode,
-	color: string
+    children: React.ReactNode
+    color: string
 }
 
-const Title = ({ children, color} : Props ) => {
-	return (
-		<div>
-			<h1 className={`font-semibold text-${color}-500 xl:text-7xl`}>
-				{children}
-			</h1>
-		</div>
-	)
+const Title = ({ children, color }: Props) => {
+    return (
+        <div>
+            <h1
+                className={`text-3xl font-semibold text-${color}-500 lg:text-7xl`}
+            >
+                {children}
+            </h1>
+        </div>
+    )
 }
 
 export default Title
